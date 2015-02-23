4132991d6bbf52f8bb9c56308402d4a68bcc89d4
aleksey <mattytedd@gmail.com>
2015-02-23 21:32:09 +0300

    Added gulp task for watching changes in javascript and writing them to lib/main.js file

--- src/main/resources/public/gulpfile.js
+++ src/main/resources/public/gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 var bower = require('main-bower-files');
 var filter = require('gulp-filter');
-var concat = require('gulp-concat');
+var concat = require('gulp-concat-util');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
 
-gulp.task('default', function() {
-    return gulp.src(bower())
-        .pipe(filter(['*.js']))
-        .pipe(uglify())
+gulp.task('scripts', function() {
+    return gulp.src(['app/**/*.js'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(concat.header('(function() {\n\'use strict\';\n'))
+        .pipe(concat.footer('\n})();\n'))
         .pipe(concat('main.js'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('lib/'));
 });
+
+gulp.task('live', function() {
+    gulp.watch('lib/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['scripts', 'live']);
