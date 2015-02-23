c8a2a277d43160c0d915a800347be7c4b45912e6
Aleksey <mattytedd@gmail.com>
2015-02-23 21:49:27 +0300

    Update gulpfile.js

--- src/main/resources/public/gulpfile.js
+++ src/main/resources/public/gulpfile.js
@@ -10,15 +10,27 @@ gulp.task('scripts', function() {
     return gulp.src(['app/**/*.js'])
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(concat.header('(function() {\n\'use strict\';\n'))
-        .pipe(concat.footer('\n})();\n'))
+        //.pipe(concat.header('(function() {\n\'use strict\';\n'))
+        //.pipe(concat.footer('\n})();\n'))
         .pipe(concat('main.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('bower-scripts', function() {
+    return gulp.src(['bower_components/**/*.js',
+    'node_components/**/*.js'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(concat('bower-main.js'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('lib/'));
 });
 
 gulp.task('live', function() {
     gulp.watch('lib/**/*.js', ['scripts']);
+    gulp.watch('bower_components/**/*.js', ['bower-scripts']);
+    gulp.watch('node_modules/**/*.js', ['bower-scripts']);
 });
 
-gulp.task('default', ['scripts', 'live']);
+gulp.task('default', ['scripts', 'bower-scripts', 'live']);
