eda3c707d2a705673c38a16527bf25c91cfeac6c
mr.sadvit <mr.sadvit@gmail.com>
2015-03-12 19:58:56 +0300

    Sorting in tables

--- src/main/resources/public/app/area/profile/ProfileController.js
+++ src/main/resources/public/app/area/profile/ProfileController.js
@@ -7,6 +7,11 @@ function ProfileController(UsersResource, $cookieStore) {
 ProfileController.prototype.init = function() {
     var self = this;
     self.user = self.cookies.get('user');
+    if (self.user.employee != undefined) {
+        self.role = self.user.employee.role;
+    } else {
+        self.role = 'MANAGER';
+    }
 };
 
 angular.module('spwizzard').controller('ProfileController', ProfileController);
